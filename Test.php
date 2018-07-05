17304ff58da873456789fe6c4767dbb755660b56
KostenkoA <kostenkoam1212@gmail.com>
2018-07-05 16:58:19 +0300

    add return type hint

---
+++ Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+/**
+ * Class for testing of the migration tool.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+class Test
+{
+    /**
+     * This is first test method.
+     *
+     * @param float     $param1
+     * @param string    $param2
+     * @param null|int  $param3
+     * @return  void
+     */
+    public function testMethod($param1, $param2, $param3)
+    {
+    }
+
+    /**
+     * This is second test method.
+     *
+     * @param null|object $data
+     * @return array
+     */
+    public function test($data)
+    {
+    }
+}
